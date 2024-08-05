33b6987700abf2f0d740ae962c13624d97137a95
Evan Davis <evandavis427@gmail.com>
2024-08-05 11:51:58 -0600

    added and partial and refactored the flight model and tests to remove assignment of a variable in the view

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -7,6 +7,13 @@ class Flight < ApplicationRecord
       joins(:airline, :passengers)
         .select('flights.*, airlines.name as airline_name, flight_passengers.passenger_id as passenger_id, passengers.name as passenger_name')
         .group_by(&:id)
+        .transform_values do |records|
+          flight = records.first
+          {
+            flight: flight,
+            passengers: records.map { |r| { name: r.passenger_name, id: r.passenger_id } }
+          }
+        end
     end
 
     def remove_passenger(passenger)
--- spec/models/flight_spec.rb
+++ spec/models/flight_spec.rb
@@ -13,39 +13,59 @@ RSpec.describe Flight, type: :model do
       @airline2 = Airline.create!(name: "Delta")
 
       @flight1 = @airline1.flights.create!(number: "1727", date: "01/15/24", departure_city: "Gotham", arrival_city: "Metropolis")
-      @flight2 = @airline2.flights.create!(number: "1828", date: "02/20/24", departure_city: "Hogsmeade", arrival_city: "Hogwarts")
+      @flight2 = @airline1.flights.create!(number: "1828", date: "02/20/24", departure_city: "Hogsmeade", arrival_city: "Hogwarts")
+      @flight3 = @airline2.flights.create!(number: "1938", date: "03/25/24", departure_city: "Springfield", arrival_city: "Shelbyville")
 
       @passenger1 = Passenger.create!(name: "Joe Exotic", age: 57)
       @passenger2 = Passenger.create!(name: "Mary Poppins", age: 30)
       @passenger3 = Passenger.create!(name: "Walter White", age: 52)
       @passenger4 = Passenger.create!(name: "John Wick", age: 45)
       @passenger5 = Passenger.create!(name: "Little Kid", age: 10)
+      @passenger6 = Passenger.create!(name: "Teen Kid", age: 17)
 
       FlightPassenger.create!(flight: @flight1, passenger: @passenger1)
       FlightPassenger.create!(flight: @flight1, passenger: @passenger2)
-      FlightPassenger.create!(flight: @flight1, passenger: @passenger3)
       FlightPassenger.create!(flight: @flight1, passenger: @passenger5)
-      FlightPassenger.create!(flight: @flight2, passenger: @passenger2)
+      FlightPassenger.create!(flight: @flight2, passenger: @passenger1)
       FlightPassenger.create!(flight: @flight2, passenger: @passenger3)
-      FlightPassenger.create!(flight: @flight2, passenger: @passenger4)
-      FlightPassenger.create!(flight: @flight2, passenger: @passenger5)
+      FlightPassenger.create!(flight: @flight3, passenger: @passenger4)
+      FlightPassenger.create!(flight: @flight3, passenger: @passenger6)
     end
 
     it 'returns flights with their airline name and passenger names' do
-      flights = Flight.with_airline_and_passengers
+      flights_with_details = Flight.with_airline_and_passengers
 
-      expect(flights.keys).to include(@flight1.id)
-      expect(flights.keys).to include(@flight2.id)
+      flight1_data = flights_with_details[@flight1.id]
+      flight2_data = flights_with_details[@flight2.id]
+      flight3_data = flights_with_details[@flight3.id]
 
-      flight1_data = flights[@flight1.id]
-      expect(flight1_data.first.number).to eq(@flight1.number)
-      expect(flight1_data.first.airline_name).to eq(@airline1.name)
-      expect(flight1_data.map(&:passenger_name)).to match_array(["Joe Exotic", "Mary Poppins", "Walter White", "Little Kid"])
+      expect(flight1_data[:flight].number).to eq(@flight1.number)
+      expect(flight1_data[:flight].airline_name).to eq(@airline1.name)
+      passenger_names_flight1 = flight1_data[:passengers].map { |p| p[:name] }
+      expect(passenger_names_flight1).to include(@passenger1.name)
+      expect(passenger_names_flight1).to include(@passenger2.name)
+      expect(passenger_names_flight1).to include(@passenger5.name)
 
-      flight2_data = flights[@flight2.id]
-      expect(flight2_data.first.number).to eq(@flight2.number)
-      expect(flight2_data.first.airline_name).to eq(@airline2.name)
-      expect(flight2_data.map(&:passenger_name)).to match_array(["Mary Poppins", "Walter White", "John Wick", "Little Kid"])
+      expect(flight2_data[:flight].number).to eq(@flight2.number)
+      expect(flight2_data[:flight].airline_name).to eq(@airline1.name)
+      passenger_names_flight2 = flight2_data[:passengers].map { |p| p[:name] }
+      expect(passenger_names_flight2).to include(@passenger1.name)
+      expect(passenger_names_flight2).to include(@passenger3.name)
+
+      expect(flight3_data[:flight].number).to eq(@flight3.number)
+      expect(flight3_data[:flight].airline_name).to eq(@airline2.name)
+      passenger_names_flight3 = flight3_data[:passengers].map { |p| p[:name] }
+      expect(passenger_names_flight3).to include(@passenger4.name)
+      expect(passenger_names_flight3).to include(@passenger6.name)
+    end
+  end
+
+  describe '#remove_passenger' do
+    before :each do
+      @airline1 = Airline.create!(name: "Frontier")
+      @flight1 = @airline1.flights.create!(number: "1727", date: "01/15/24", departure_city: "Gotham", arrival_city: "Metropolis")
+      @passenger1 = Passenger.create!(name: "Joe Exotic", age: 57)
+      FlightPassenger.create!(flight: @flight1, passenger: @passenger1)
     end
 
     it 'removes a passenger from a flight' do
